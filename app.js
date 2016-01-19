d372be0eee883147d9b4afc17b9d2e63801d22f3
Rita Rafaeli <ritarafaeli@users.noreply.github.com>
2016-01-19 12:52:26 -0500

    update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', function($scope) {
 
-    $scope.name = '';
-
+    $scope.projects = [
+        {title: "Project 2", github: 'https://github.com/ritarafaeli/p2', live: 'http://www.p2.ritarafaeli.me'},
+        {title: "Project 3", github: 'https://github.com/ritarafaeli/p3', live: 'http://www.p3.ritarafaeli.me'},
+        {title: "Project 4", github: 'https://github.com/ritarafaeli/p4', live: 'http://www.p4.ritarafaeli.me'}  
+    ];
 }]);
