debe410b022a96d50f344ddede2191a66cfbd31f
Rita Rafaeli <ritarafaeli@users.noreply.github.com>
2016-02-05 14:41:10 -0500

    update live urls again

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var myApp = angular.module('myApp', []);
 myApp.controller('mainController', ['$scope', function($scope) {
 
     $scope.projects = [
-        {title: "Project 2", github: 'https://github.com/ritarafaeli/p2', live: 'p2.ritarafaeli.me'},
-        {title: "Project 3", github: 'https://github.com/ritarafaeli/p3', live: 'p3.ritarafaeli.me'},
-        {title: "Project 4", github: 'https://github.com/ritarafaeli/p4', live: 'p4.ritarafaeli.me'}  
+        {title: "Project 2", github: 'https://github.com/ritarafaeli/p2', live: 'http://p2.ritarafaeli.me'},
+        {title: "Project 3", github: 'https://github.com/ritarafaeli/p3', live: 'http://p3.ritarafaeli.me'},
+        {title: "Project 4", github: 'https://github.com/ritarafaeli/p4', live: 'http://p4.ritarafaeli.me'}  
     ];
 }]);
